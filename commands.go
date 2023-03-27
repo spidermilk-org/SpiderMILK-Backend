32318dca5ebee7f7d1eae35aee3b59c350ce572b
PhilVoel <philipp123.voeller@gmail.com>
2023-03-27 17:10:50 +0200

    Since "help" now runs client-side we don't need to handle it on the server anymore

--- commands.go
+++ commands.go
@@ -3,7 +3,6 @@ package main
 func commandHandler(command string) string {
 	switch(command) {
 		case "help":
-			return `"clear" to clear the screen<br>"exit" to exit the game<br>"help" to print help message`
 		case "clear":
 		case "exit":
 			return "Don't do that"
